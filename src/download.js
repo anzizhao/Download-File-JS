15d48ce232d786331a43951b5f53b1eeb7194891
anzizhao <anzizhao@126.com>
2015-12-28 10:46:32 +0800

    增强浏览器的判断

--- src/download.js
+++ src/download.js
@@ -1,26 +1,32 @@
-window.downloadFile = function (sUrl, filename) {
+( function(){
 
-    //iOS devices do not support downloading. We have to inform user about this.
-    if (/(iP)/g.test(navigator.userAgent)) {
-        alert('Your device does not support files downloading. Please try again in desktop browser.');
-        return false;
-    }
+    window.downloadFile = function (sUrl, filename) {
+        //iOS devices do not support downloading. We have to inform user about this.
+        if (/(iP)/g.test(navigator.userAgent)) {
+            alert('Your device does not support files downloading. Please try again in desktop browser.');
+            return false;
+        }
 
     //If in Chrome or Safari - download via virtual link click
     if (window.downloadFile.isChrome || window.downloadFile.isSafari) {
         //Creating new link node.
         var link = document.createElement('a');
         link.href = sUrl;
-        if( filename ){ 
-            link.donwload =  filename;
-        }
-
+        console.dir(link);
+        console.log('download ' + link.download);
         if (link.download !== undefined) {
-            //Set HTML5 download attribute. This will prevent file from opening if supported.
-            var fileName = sUrl.substring(sUrl.lastIndexOf('/') + 1, sUrl.length);
-            link.download = fileName;
-        }
 
+            console.log('a element click' + link.download + sUrl);
+            if( filename ){ 
+                link.download =  filename;
+            } else {
+                //Set HTML5 download attribute. This will prevent file from opening if supported.
+                var fileNameUrl = sUrl.substring(sUrl.lastIndexOf('/') + 1, sUrl.length);
+                link.download = fileNameUrl;
+            }
+
+        }
+        //console.log('a element click' + link.download + sUrl);
         //Dispatching click event.
         if (document.createEvent) {
             var e = document.createEvent('MouseEvents');
@@ -35,9 +41,38 @@ window.downloadFile = function (sUrl, filename) {
         sUrl += '?download';
     }
 
+    //console.log('window.open' + sUrl);
     window.open(sUrl, '_self');
     return true;
 }
 
-window.downloadFile.isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-window.downloadFile.isSafari = navigator.userAgent.toLowerCase().indexOf('safari') > -1;
+    var Sys = {};
+    var ua = navigator.userAgent.toLowerCase();
+
+    //拦截判断 一些基于chrome 修改浏览器不支持html5 a标签download
+    if( ua.indexOf('xiaomi') > -1){
+        console.log("XiaoMi");
+
+    } else {
+
+        if (window.ActiveXObject){
+            Sys.ie = ua.match(/msie ([\d.]+)/)[1]
+        } else if (document.getBoxObjectFor){
+            Sys.firefox = ua.match(/firefox\/([\d.]+)/)[1]
+        } else if (window.MessageEvent && !document.getBoxObjectFor){
+
+            Sys.chrome = ua.match(/chrome\/([\d.]+)/)[1]
+        } else if (window.opera) {
+            Sys.opera = ua.match(/opera.([\d.]+)/)[1]
+        } else if (window.openDatabase) {
+            Sys.safari = ua.match(/version\/([\d.]+)/)[1];
+        }
+
+        if ( Sys.chrome ) {
+            window.downloadFile.isChrome = true;
+        } else  if ( Sys.safari ){
+            window.downloadFile.isSafari = true;
+        }
+    } 
+
+} ());
